807f3469604bfe2b0c12a194bf0bbb4b92c21738
Artemiy Vishnevskiy <temaa.mann@gmail.com>
2019-10-20 01:20:02 +0300

    prototyping filterable tree

--- lib/src/main/java/io/sourcherry/kdtree/KdTree.kt
+++ lib/src/main/java/io/sourcherry/kdtree/KdTree.kt
@@ -1,28 +1,59 @@
 package io.sourcherry.kdtree
 
-class Node<T>(
+ class Node<T>(
     var data: T,
     var left: Node<T>? = null,
     var right: Node<T>? = null
 )
 
+class Bound<B : Comparable<B>>(
+    val dimension: Int,
+    val range: ClosedRange<B>
+)
 
+
+/*
+https://github.com/mgruben/Kd-Trees/blob/master/KdTree.java
+ */
 class KdTree<I>(
-    private vararg val dinemtions: Comparator<I>
+    private val dimensions: List<Comparator<I>>
 ) {
 
     var root: Node<I>? = null
 
     fun add(item: I) {
-        if (root == null) root = Node(item)
-        addTraversal(item)
+        check(dimensions.isNotEmpty()) { "No dimension comparators provided" }
+        root = insert(root, item, 0)
     }
 
-    fun findAll() {
+    fun findAll(bounds: List<Bound<*>>): Collection<I> {
 
+
+        return emptyList()
     }
 
-    private fun addTraversal(item: I) {
-        check(dinemtions.isNotEmpty()) { "No dimension comparators provided" }
+    private fun insert(node: Node<I>?, item: I, dimension: Int): Node<I> {
+        if (node == null) {
+            return Node(item)
+        } else {
+            val cmp = dimensions[dimension].compare(node.data, item)
+
+            if (cmp < 0) {
+                node.left = insert(node.left, item, dimension.next())
+            }
+
+            if (cmp > 0) {
+                node.right = insert(node.right, item, dimension.next())
+            }
+
+            if (cmp == 0) {
+                if (node.data != item) {
+                    node.right = insert(node.right, item, dimension.next())
+                }
+            }
+            return node
+        }
     }
+
+    private fun Int.next(): Int = (this + 1) % dimensions.size
 }
--- lib/src/main/java/io/sourcherry/kdtree/Main.kt
+++ lib/src/main/java/io/sourcherry/kdtree/Main.kt
@@ -1,10 +1,80 @@
 package io.sourcherry.kdtree
 
+import java.util.*
+import kotlin.Comparator
+import kotlin.random.Random
+
 
 object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
 
+        val tree = KdTree(
+            listOf(
+                Comparators.byPrice,
+                Comparators.byDate,
+                Comparators.byDuration
+            )
+        )
+
+        fillTree(tree)
+    }
+
+    private fun fillTree(tree: KdTree<Ticket>) {
+        for (i in 0 until 100) {
+            tree.add(genTicket())
+        }
+    }
+
+    private fun genTicket(): Ticket {
+        val date = Date(System.currentTimeMillis() + Random.nextLong(0, 1000000))
+        val duration = Random.nextInt(0, 200000)
+
+        return Ticket(
+            date = date,
+            agency = "OneTwoGo",
+            duration = duration,
+            tariff = genTariff()
+        )
+    }
+
+    private fun genTariff(): Tariff {
+        val type = Random.nextInt(0, 3).apply {
+
+        Tariff.Type.ECONOM
+        }
+        val price = Random.nextInt(50, 5000)
+        return Tariff(type, price)
+    }
+
+    data class Ticket(
+        val date: Date,
+        val agency: String,
+        val duration: Int,
+        val tariff: Tariff
+    )
+
+    data class Tariff(
+        val type: Type,
+        val price: Int
+    ) {
+        enum class Type {
+            ECONOM, LUX, BUSNESS, FIRST_CLASS
+        }
+    }
+
+    object Comparators {
+        val byPrice = Comparator<Ticket> { ticket1, ticket2 ->
+            compareValuesBy(ticket1.tariff.price, ticket2.tariff.price)
+        }
+
+        val byDate = Comparator<Ticket> { ticket1, ticket2 ->
+            compareValuesBy(ticket1.date, ticket2.date)
+        }
+
+        val byDuration = Comparator<Ticket> { ticket1, ticket2 ->
+            compareValuesBy(ticket1.duration, ticket2.duration)
+        }
     }
 }
