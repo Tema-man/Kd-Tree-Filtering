70655b0ad2f2a0f85363d8fff2f468e59ee8f627
Artemiy Vishnevskiy <temaa.mann@gmail.com>
2019-10-21 12:29:46 +0300

    WIP prototyping find range method

--- lib/src/main/java/io/sourcherry/kdtree/KdTree.kt
+++ lib/src/main/java/io/sourcherry/kdtree/KdTree.kt
@@ -1,39 +1,79 @@
 package io.sourcherry.kdtree
 
- class Node<T>(
+import java.util.*
+
+
+class Node<T>(
     var data: T,
     var left: Node<T>? = null,
     var right: Node<T>? = null
 )
 
-class Bound<B : Comparable<B>>(
-    val dimension: Int,
-    val range: ClosedRange<B>
-)
-
+interface Bound<I> {
+    val dimension: Comparator<I>
+    fun contains(item: I): Boolean
+    fun intersects(bound: Bound<I>): Boolean
+}
 
 /*
 https://github.com/mgruben/Kd-Trees/blob/master/KdTree.java
  */
 class KdTree<I>(
-    private val dimensions: List<Comparator<I>>
+    private val dimensions: List<Comparator<I>>,
+    items: Collection<I> = emptyList()
 ) {
 
-    var root: Node<I>? = null
+    private val dimensMap: Map<Class<*>, Comparator<I>>
+    private var size = 0L
+
+    init {
+        items.forEach { add(it) }
+        dimensMap = dimensions.map { it::class.java to it }.toMap()
+    }
+
+    private var root: Node<I>? = null
 
     fun add(item: I) {
         check(dimensions.isNotEmpty()) { "No dimension comparators provided" }
         root = insert(root, item, 0)
     }
 
-    fun findAll(bounds: List<Bound<*>>): Collection<I> {
 
+    /**
+     * Return range of items that contains in specified
+     *
+     * TODO: Add bounds ordering
+     *
+     */
+    fun range(bounds: List<Bound<I>>): Collection<I> {
+        val items = Stack<I>()
+        if (root == null) return items
 
-        return emptyList()
+        val nodes = Stack<Node<I>>()
+        nodes.push(root)
+        while (!nodes.isEmpty()) {
+
+            // Examine the next Node
+            val node = nodes.pop()
+
+            // Add contained points to our points stack
+            if (bounds.any { it.contains(node.data) }) items.push(node.data)
+
+            //Add Nodes containing promising rectangles to our nodes stack.
+            if (node.left != null && rect.intersects(node.lb.rect)) {
+                nodes.push(node.left)
+            }
+
+            if (node.right != null && rect.intersects(node.rt.rect)) {
+                nodes.push(node.right)
+            }
+        }
+        return items
     }
 
     private fun insert(node: Node<I>?, item: I, dimension: Int): Node<I> {
         if (node == null) {
+            size++
             return Node(item)
         } else {
             val cmp = dimensions[dimension].compare(node.data, item)
